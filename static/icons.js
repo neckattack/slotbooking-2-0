880e05df62118e71487d6bb4e41403650a66978a
neckattack <chris@neckattack.net>
2026-01-13 07:55:06 +0100

    Integrate Lucide icons and use in inbox hover actions

---
+++ static/icons.js
@@ -0,0 +1,9 @@
+// Zentrale Icon-Registry für das Frontend
+window.ICON_REGISTRY = {
+  mail: "mail",
+  user: "user",
+  eye: "eye",
+  archive: "archive",
+  reload: "refresh-ccw",
+  filter: "sliders-horizontal",
+};
