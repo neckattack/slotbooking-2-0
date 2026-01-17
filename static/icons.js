510d5cdc780edde429a18d34c1c22757f7b43467
neckattack <chris@neckattack.net>
2026-01-17 17:56:30 +0100

    Guard qcTone usage in quick card to avoid ReferenceError

--- static/icons.js
+++ static/icons.js
@@ -36,6 +36,7 @@ window.ICON_REGISTRY = {
     "chevron-up": '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m18 15-6-6-6 6"/></svg>',
     "chevron-down": '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m6 9 6 6 6-6"/></svg>',
     paperclip: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21.44 11.05 12 20.5a5 5 0 0 1-7.07-7.07l9.19-9.19a3.5 3.5 0 0 1 4.95 4.95L9.88 18.38a2 2 0 0 1-2.83-2.83L15.07 7.5"/></svg>',
+    languages: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m5 8 6 6"/><path d="m4 14 6-6 2-3"/><path d="M2 5h12"/><path d="M7 2h1"/><path d="m16 13 3 5 3-5"/><path d="M19 11v1"/><path d="M19 18v1"/></svg>',
   };
 
   window.getIconSvg = function(name, props){
